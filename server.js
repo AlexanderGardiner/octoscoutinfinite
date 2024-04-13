9fadb8404aec67109f6978bab71506f756fde7e1
AlexanderGardiner <misc@alexandergardiner.com>
2024-04-13 14:45:07 -0700

    inital commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+app.post("/submitData", (req, res) => {
+  res.status(200);
+  res.send();
+});
+const PORT = 9084;
+app.listen(PORT, () => {
+  console.log("App is listening on port 9084");
+});
