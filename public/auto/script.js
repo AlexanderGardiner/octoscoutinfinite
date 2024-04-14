b1117b390defcacd628b5d20d08ecbf754fd0a26
AlexanderGardiner <misc@alexandergardiner.com>
2024-04-13 21:05:11 -0700

    auto page fleshed out

--- public/auto/script.js
+++ public/auto/script.js
@@ -7,12 +7,15 @@ import {
 let JSONConfig = await getJSONConfig();
 let fieldContainer = document.getElementById("fieldContainer");
 let fieldImage = document.getElementById("fieldImage");
+let gamePieceViewer = document.getElementById("gamePieceContainer");
 let gamePieces = [];
 generateStartingLocationButtons();
 
 class GamePiece {
-  constructor(collectionLocation) {
+  constructor(collectionLocation, name, color) {
     this.collectionLocation = collectionLocation;
+    this.name = name;
+    this.color = color;
   }
 }
 
@@ -21,20 +24,27 @@ function generateStartingLocationButtons() {
   let gamePieces = JSONConfig.gamePieces;
 
   for (let i = 0; i < gamePieces.length; i++) {
+    let blueButtonColorIterator = 75;
     for (let j = 0; j < gamePieces[i].blueAutoCollectionLocations.length; j++) {
       addCollectionButton(
         gamePieces[i].blueAutoCollectionLocations[j],
-        gamePieces[i].name
+        gamePieces[i].name,
+        "rgb(" + [0, 0, blueButtonColorIterator].join(",") + ")"
       );
+      blueButtonColorIterator +=
+        180 / gamePieces[i].blueAutoCollectionLocations.length;
     }
-
+    let redButtonColorIterator = 75;
     for (let j = 0; j < gamePieces[i].redAutoCollectionLocations.length; j++) {
       addCollectionButton(
         gamePieces[i].redAutoCollectionLocations[j],
-        gamePieces[i].name
+        gamePieces[i].name,
+        "rgb(" + [redButtonColorIterator, 0, 0].join(",") + ")"
       );
+      redButtonColorIterator +=
+        180 / gamePieces[i].redAutoCollectionLocations.length;
     }
-
+    let neutralButtonColorIterator = 75;
     for (
       let j = 0;
       j < gamePieces[i].neutralAutoCollectionLocations.length;
@@ -42,16 +52,19 @@ function generateStartingLocationButtons() {
     ) {
       addCollectionButton(
         gamePieces[i].neutralAutoCollectionLocations[j],
-        gamePieces[i].name
+        gamePieces[i].name,
+        "rgb(" + [0, neutralButtonColorIterator, 0].join(",") + ")"
       );
+      neutralButtonColorIterator +=
+        180 / gamePieces[i].neutralAutoCollectionLocations.length;
     }
   }
 }
 
-function addCollectionButton(collectionLocation, gamePieceName) {
+function addCollectionButton(collectionLocation, gamePieceName, buttonColor) {
   let button = document.createElement("img");
   button.onclick = function () {
-    collectPiece(collectionLocation);
+    collectPiece(collectionLocation, gamePieceName, buttonColor);
   };
 
   button.classList.add("positionButton");
@@ -63,9 +76,48 @@ function addCollectionButton(collectionLocation, gamePieceName) {
   fieldContainer.appendChild(button);
   button.style.position = "absolute";
   button.src = "/images/" + gamePieceName + ".png";
+  button.style.backgroundColor = buttonColor;
 }
 
-function collectPiece(location) {
-  gamePieces.push(new GamePiece(location));
-  console.log(gamePieces);
+function collectPiece(location, gamePieceName, buttonColor) {
+  gamePieces.push(new GamePiece(location, gamePieceName, buttonColor));
+  updateGamePieceViewer();
+}
+
+function updateGamePieceViewer() {
+  gamePieceViewer.innerHTML = "";
+  for (let i = 0; i < gamePieces.length; i++) {
+    let possibleResults = [];
+    for (let j = 0; j < JSONConfig.gamePieces.length; j++) {
+      if (JSONConfig.gamePieces[j].name == gamePieces[i].name) {
+        possibleResults = JSONConfig.gamePieces[j].autoPossibleResults;
+      }
+    }
+    let gamePieceContainer = document.createElement("div");
+    gamePieceContainer.style.alignItems = "center";
+    gamePieceContainer.style.display = "flex";
+    let gamePieceImage = document.createElement("img");
+    gamePieceImage.src = "/images/" + gamePieces[i].name + ".png";
+    gamePieceImage.style.width = "5vh";
+    gamePieceImage.style.height = "5vh";
+    gamePieceImage.style.margin = "5px";
+    gamePieceImage.style.top = "0px";
+
+    let gamePieceResultSelector = document.createElement("select");
+    gamePieceResultSelector.style.margin = "5px";
+    gamePieceResultSelector.style.top = "0px";
+    for (var j = 0; j < possibleResults.length; j++) {
+      var option = document.createElement("option");
+      option.value = possibleResults[j].name;
+      option.text = possibleResults[j].name;
+      gamePieceResultSelector.appendChild(option);
+    }
+    gamePieceContainer.appendChild(gamePieceImage);
+    gamePieceContainer.appendChild(gamePieceResultSelector);
+    gamePieceViewer.appendChild(gamePieceContainer);
+    gamePieceContainer.style.backgroundColor = gamePieces[i].color;
+
+    document.getElementById("gamePieceViewer").scrollTop =
+      document.getElementById("gamePieceViewer").scrollHeight;
+  }
 }
