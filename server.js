71f10f14ec13200c84f441b115ce1891774d0b73
AlexanderGardiner <misc@alexandergardiner.com>
2024-04-16 20:43:26 -0700

    Threading issues mitigated

--- server.js
+++ server.js
@@ -11,32 +11,54 @@ app.use(
 );
 app.use(express.static("public"));
 
+const retryOptions = {
+  retries: {
+    retries: 5,
+    factor: 3,
+    minTimeout: 1 * 1000,
+    maxTimeout: 60 * 1000,
+    randomize: true,
+  },
+};
 app.post("/submitData", async (req, res) => {
   console.log(req.body);
   await writeDataToJSON(req.body);
   res.sendStatus(200);
 });
+app.get("/output.json", async (req, res) => {
+  const filePath = "./output.json";
+  let data;
+  // Acquire lock
+  await fileLock.lock(filePath, retryOptions).then((release) => {
+    // Read the JSON file
+    data = fs.readFileSync(filePath, "utf8");
+    return release();
+  });
+  // Set headers to indicate JSON response
+  res.setHeader("Content-Type", "application/json");
+  // Send JSON data as response
+  res.send(data);
+});
+
+async function writeDataToJSON(data) {
+  const filePath = "./output.json";
+  data.timestamp = new Date();
+  await fileLock.lock(filePath, retryOptions).then((release) => {
+    // Read existing file content
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    const jsonData = JSON.parse(fileContent || "[]");
+
+    // Append new data
+    jsonData.push(data);
+
+    // Write updated data to file
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+    console.log("Data appended to output.json.");
+    return release();
+  });
+}
 
 const PORT = 9084;
 app.listen(PORT, () => {
   console.log("App is listening on port 9084");
 });
-
-async function writeDataToJSON(data) {
-  let filePath = "./public/output.json";
-  data.timestamp = new Date();
-  const lockfilePath = filePath + ".lock";
-
-  try {
-    await fileLock.lock(lockfilePath);
-    const fileContent = fs.readFileSync(filePath, "utf8");
-    const jsonData = JSON.parse(fileContent || "[]");
-    jsonData.push(data);
-    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
-    console.log("Data appended to output.json.");
-  } catch (err) {
-    console.error("Error writing to output.json:", err);
-  } finally {
-    await fileLock.unlock(lockfilePath);
-  }
-}
